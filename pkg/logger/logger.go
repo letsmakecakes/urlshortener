d99ba16a0c81053b2cf9e8952fcd1b3c77358e0a
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-01-02 16:14:21 +0530

    Added logger file.

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,46 @@
 package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var log *zap.Logger
+
+// Initialize sets up the logger based on the environment
+func Initialize(environment string) error {
+	var config zap.Config
+
+	if environment == "production" {
+		config = zap.NewDevelopmentConfig()
+		config.EncoderConfig.TimeKey = "timestamp"
+		config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	} else {
+		config = zap.NewDevelopmentConfig()
+		config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+
+	var err error
+	log, err = config.Build()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetLogger returns the configured logger instance
+func GetLogger() *zap.Logger {
+	if log == nil {
+		log, _ = zap.NewDevelopment()
+	}
+	return log
+}
+
+// Sync flushes any buffered log entries
+func Sync() error {
+	if log != nil {
+		return log.Sync()
+	}
+	return nil
+}
