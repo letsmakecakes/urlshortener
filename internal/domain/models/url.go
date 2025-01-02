d25a213dc5391055a6dc22fba93729e525972913
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-01-02 16:16:02 +0530

    Added data model for URL.

--- internal/domain/models/url.go
+++ internal/domain/models/url.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type URL struct {
+	ID          string    `json:"id" bson:"_id,omitempty"`
+	OriginalURL string    `json:"original_url" bson:"original_url"`
+	ShortCode   string    `json:"short_code" bson:"short_code"`
+	AccessCount int       `json:"access_count" bson:"access_count"`
+	CreatedAt   time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" bson:"updated_at"`
+}
