a0be04c1d6b215b8bef07609556428bcf471554e
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-01-02 16:16:53 +0530

    Added interface for database operations.

--- internal/domain/repositories/url_repository.go
+++ internal/domain/repositories/url_repository.go
@@ -1,1 +1,14 @@
 package repositories
+
+import (
+	"context"
+	"urlshortener/internal/domain/models"
+)
+
+type URLRepository interface {
+	CreateURL(ctx context.Context, url *models.URL) error
+	GetURLByShortCode(ctx context.Context, shortCode string) (*models.URL, error)
+	UpdateURL(ctx context.Context, url *models.URL) error
+	DeleteURL(ctx context.Context, shortCode string) error
+	IncrementURLAccessCount(ctx context.Context, shortCode string) error
+}
