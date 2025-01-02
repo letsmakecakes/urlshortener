fdcecf8042e9f1b12caa6dd81cdc82bac1a27327
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-01-02 16:15:52 +0530

    Added code to generate short code.

--- internal/pkg/generator/shortcode.go
+++ internal/pkg/generator/shortcode.go
@@ -1,1 +1,29 @@
 package generator
+
+import (
+	"math/rand"
+)
+
+const (
+	charset    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	codeLength = 6
+)
+
+// GenerateShortCode creates a random short code of a specified length using the defined character set.
+func GenerateShortCode() string {
+	code := make([]byte, codeLength)
+	for i := range code {
+		code[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(code)
+}
+
+// GenerateShortCodeWithSeed creates a random short code using a local rando generator with a specified seed.
+func GenerateShortCodeWithSeed(seed int64) string {
+	rng := rand.New(rand.NewSource(seed))
+	code := make([]byte, codeLength)
+	for i := range code {
+		code[i] = charset[rng.Intn(len(charset))]
+	}
+	return string(code)
+}
